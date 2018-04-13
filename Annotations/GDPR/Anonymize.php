290019d642d8071df7fcc453b147f81721e6ec5b
Tom Schillemans <ts@superbrave.nl>
2018-04-13 12:42:32 +0200

    Added more documentation and some extra types to anonymize annotation

--- Annotations/GDPR/Anonymize.php
+++ Annotations/GDPR/Anonymize.php
@@ -4,16 +4,43 @@ use Doctrine\Common\Annotations\Annotation;
 use Doctrine\Common\Annotations\AnnotationException;
 
 /**
- * Class Anonymize
- *
+ * Annotation to flag an entity field to be anonymized
+ * to conform with the GDPR right to be forgotten
+ * 
  * @Annotation()
  * @Annotation\Target({"PROPERTY"})
  */
 class Anonymize
 {
+    /**
+     * Alias of TYPE_CONCAT
+     */
     const TYPE_STRING = 'string';
+
+    /**
+     * Anonymize a string to <columnName>-<primaryKey>
+     */
+    const TYPE_CONCAT = 'concat';
+
+    /**
+     * Anonymize a field by emptying it
+     * Alias of TYPE_FIXED with an empty value.
+     */
+    const TYPE_TRUNCATE = 'truncate';
+
+    /**
+     * Anonymize an IP field by filling in 127.0.0.1
+     * If this is a string field, INET_ATON will be used instead.
+     */
     const TYPE_IP = 'ip';
+
+    /**
+     * Anonymize by providing a fixed value
+     * Requires the value property to be given
+     */
     const TYPE_FIXED = 'fixed';
+
+    /** Anonymize an email to "<primaryKey>@example.com" */
     const TYPE_EMAIL = 'email';
 
     /**
@@ -22,6 +49,8 @@ class Anonymize
      *     Anonymize::TYPE_IP
      *     Anonymize::TYPE_FIXED
      *     Anonymize::TYPE_EMAIL
+     *     Anonymize::TYPE_CONCAT
+     *     Anonymize::TYPE_TRUNCATE
      * })
      * @var string
      */
@@ -40,5 +69,3 @@ class Anonymize
         }
     }
 }
-
-// @GDPR\Anonymize(type='fixed', value='127.0.0.1')
