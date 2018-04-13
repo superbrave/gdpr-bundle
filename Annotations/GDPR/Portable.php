5b66ac38f781c5110842a569ab22f62e8086e9c2
Tom Schillemans <ts@superbrave.nl>
2018-04-13 12:41:46 +0200

    Created base portability annotaion

---
+++ Annotations/GDPR/Portable.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Annotation to flag an entity field to be exported
+ * to conform with the GDPR right of portability.
+ *
+ * @Annotation()
+ * @Annotation\Target({"PROPERTY"})
+ */
+class Portable
+{
+    /**
+     * Use given name instead of column name in the exported data.
+     *
+     * @var string
+     */
+    public $fieldName;
+}
