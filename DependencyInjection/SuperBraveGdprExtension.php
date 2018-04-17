f8469ec38d9e26228308fd7fb8fe244fb0e5b329
Niels Nijens <nn@superbrave.nl>
2018-04-17 16:51:16 +0200

    Add PHP CS fixer code changes

--- DependencyInjection/SuperBraveGdprExtension.php
+++ DependencyInjection/SuperBraveGdprExtension.php
@@ -1,28 +1,29 @@
 <?php
 /**
- * This file loads and manages the GDPR bundle
+ * This file loads and manages the GDPR bundle.
  *
  * Minimal required PHP version is 5.6
  *
  * @category  Bundle
- * @package   Gdpr
+ *
  * @author    Superbrave <info@superbrave.nl>
  * @copyright 2018 Superbrave <info@superbrave.nl>
  * @license   https://github.com/superbrave/gdpr-bundle/blob/master/LICENSE MIT
- * @link      https://www.superbrave.nl/
+ *
+ * @see      https://www.superbrave.nl/
  */
 
 namespace SuperBrave\GdprBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
- * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * @see http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
 class SuperBraveGdprExtension extends Extension
 {
@@ -32,7 +33,6 @@ class SuperBraveGdprExtension extends Extension
      * @param array            $configs   An array of configuration items to process
      * @param ContainerBuilder $container A DI container that provides an API to easily describe services
      *
-     * @return void
      * @throws \InvalidArgumentException When provided tag is not defined in this extension
      */
     public function load(array $configs, ContainerBuilder $container)
--- SuperBraveGdprBundle.php
+++ SuperBraveGdprBundle.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * This file initializes the GDPR bundle
+ * This file initializes the GDPR bundle.
  *
  * Minimal required PHP version is 5.6
  *
  * @category  Bundle
- * @package   Gdpr
+ *
  * @author    Superbrave <info@superbrave.nl>
  * @copyright 2018 Superbrave <info@superbrave.nl>
  * @license   https://github.com/superbrave/gdpr-bundle/blob/master/LICENSE MIT
- * @link      https://www.superbrave.nl/
+ *
+ * @see      https://www.superbrave.nl/
  */
 
 namespace SuperBrave\GdprBundle;
@@ -17,7 +18,7 @@ namespace SuperBrave\GdprBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class SuperBraveGdprBundle
+ * Class SuperBraveGdprBundle.
  */
 class SuperBraveGdprBundle extends Bundle
 {
