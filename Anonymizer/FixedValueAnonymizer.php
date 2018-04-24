e6d1991ec7320374492ad4dfb28dabbd48945148
Tom Schillemans <ts@superbrave.nl>
2018-04-24 15:01:53 +0200

    Renamed StringAnonymizer to FixedValueAnonymizer

--- Anonymizer/FixedValueAnonymizer.php
+++ Anonymizer/FixedValueAnonymizer.php
@@ -12,26 +12,60 @@
 
 namespace SuperBrave\GdprBundle\Anonymizer;
 
+use SuperBrave\GdprBundle\Manipulator\PropertyManipulator;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+
 /**
- * Class FixedValueAnonymizer
+ * Email anonymizer class
  *
  * @package SuperBrave\GdprBundle\Anonymizer
  */
 class FixedValueAnonymizer implements AnonymizerInterface
 {
+    /**
+     * Property Manipulator service
+     *
+     * @var PropertyManipulator
+     */
+    private $propertyManipulator;
+
+    /**
+     * StringAnonymizer constructor.
+     * @param PropertyManipulator $propertyManipulator
+     */
+    public function __construct(PropertyManipulator $propertyManipulator)
+    {
+        $this->propertyManipulator = $propertyManipulator;
+    }
+
+    /**
+     * @param mixed $propertyValue The value that has to be converted
+     * @param array $options Options to help the anonymizer do its job
+     *
+     * @return string               The Anonymized string
+     */
     public function anonymize($propertyValue, array $options = [])
     {
         $resolver = new OptionsResolver();
         $this->configureOptions($resolver);
         $options = $resolver->resolve($options);
 
-        return $options['annotationValue'];
+        $annotationValue = $options['annotationValue'];
+
+        $matches = [];
+        if (preg_match_all('/{(.*?)}/', $options['annotationValue'], $matches)) {
+            foreach ((array)$matches[1] as $key => $property) {
+                $wildcardValue = $this->propertyManipulator->getPropertyValue($options['object'], $property);
+                $annotationValue = str_replace($matches[0][$key], $wildcardValue, $annotationValue);
+            }
+        }
+
+        return $annotationValue;
     }
 
     private function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setRequired(['annotationValue']);
+        $resolver->setRequired(['annotationValue', 'object']);
     }
 }
--- Tests/Anonymizer/FixedValueAnonymizerTest.php
+++ Tests/Anonymizer/FixedValueAnonymizerTest.php
@@ -12,45 +12,117 @@
 
 namespace SuperBrave\GdprBundle\Tests\Anonymizer;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit_Framework_MockObject_MockObject;
 use SuperBrave\GdprBundle\Anonymizer\FixedValueAnonymizer;
+use SuperBrave\GdprBundle\Manipulator\PropertyManipulator;
+use SuperBrave\GdprBundle\Tests\AnnotatedMock;
 use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 
 /**
- * Tests the behaviour of the FixedValueAnonynizer
+ * tests the behaviour of the EmailAnonymizer
  */
-class FixedValueAnonymizerTest extends PHPUnit_Framework_TestCase
+class FixedValueAnonymizerTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * The "annotationValue" option is required for this anonymier to work
+     * The mock property accessor instance.
+     *
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $propertyManipulator;
+
+    /**
+     * An instance of StringAnonymizer being tested
+     *
+     * @var FixedValueAnonymizer
+     */
+    private $anonymizer;
+
+    /**
+     * Creates a new AnnotationNormalizer instance for testing.
      *
      * @return void
      */
-    public function testAnnotationValueIsARequiredOption()
+    public function setUp()
     {
-        $this->expectException(MissingOptionsException::class);
-        $this->expectExceptionMessage('The required option "annotationValue" is missing');
+        $this->propertyManipulator = new PropertyManipulator(
+            PropertyAccess::createPropertyAccessor()
+        );
 
-        $anonymizer = new FixedValueAnonymizer();
-        $anonymizer->anonymize('johndoe');
+        $this->anonymizer = new FixedValueAnonymizer($this->propertyManipulator);
     }
 
     /**
-     * Tests the anonymization with a fixed value
-     *
-     * The returned value hould be the same as the provided one
+     * Tests if constructing a new StringAnonymizer sets the instance properties.
      *
      * @return void
      */
-    public function testAnonymize()
+    public function testConstructHasCorrectProperty()
     {
-        $anonymizer = new FixedValueAnonymizer();
+        $this->assertAttributeSame($this->propertyManipulator, 'propertyManipulator', $this->anonymizer);
+    }
 
-        $propertyValue = 'foobar';
-        $annotationValue = 'testvalue';
+    public function testAnonymizerShouldReceiveAnnotationValueOptionOrThrowException()
+    {
+        $this->expectException(MissingOptionsException::class);
+        $this->expectExceptionMessage('The required option "annotationValue" is missing.');
 
-        $this->assertEquals($annotationValue, $anonymizer->anonymize($propertyValue, [
-            'annotationValue' => $annotationValue
+        $mock = new AnnotatedMock();
+
+        $this->anonymizer->anonymize('johndoe@appleseed.com', [
+            'object' => $mock,
+        ]);
+    }
+
+    public function testAnonymizerShouldReceiveObjectOptionOrThrowException()
+    {
+        $this->expectException(MissingOptionsException::class);
+        $this->expectExceptionMessage('The required option "object" is missing.');
+
+        $this->anonymizer->anonymize('johndoe@appleseed.com', [
+            'annotationValue' => 'foobar',
+        ]);
+    }
+
+    /**
+     * Test wether or not getting the wildcards properties will be replaced with their values
+     */
+    public function testAnonymizeStringGivenAFormatWithMultipleProperties()
+    {
+        $mock = new AnnotatedMock();
+
+        $this->assertEquals('email-1-bar@localhost', $this->anonymizer->anonymize('johndoe@appleseed.com', [
+            'annotationValue' => 'email-{baz}-{foo}@localhost',
+            'object' => $mock,
+        ]));
+    }
+
+    /**
+     * Test that an exception is thrown when the object does not have the specified property
+     */
+    public function testAnonymizeStringGivenAFormatWithInvalidPropertiesThrowsAnException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('The property "$nonexistent" does not exist on class "SuperBrave\GdprBundle\Tests\AnnotatedMock"');
+
+        $mock = new AnnotatedMock();
+
+        $this->anonymizer->anonymize('johndoe@appleseed.com', [
+            'annotationValue' => 'email-{nonexistent}',
+            'object' => $mock,
+        ]);
+    }
+
+    /**
+     * Test that the value is returned if there aren't any wildcards
+     */
+    public function testAnonymizeStringGivenAFormatWithNoWildcardReturnsTheString()
+    {
+        $mock = new AnnotatedMock();
+
+        $this->assertEquals('foobarbaz', $this->anonymizer->anonymize('johndoe@appleseed.com', [
+            'annotationValue' => 'foobarbaz',
+            'object' => $mock,
         ]));
     }
 }
