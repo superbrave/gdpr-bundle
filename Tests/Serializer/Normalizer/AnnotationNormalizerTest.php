58a93ad9454efdf449acb9dce5314bc9fde58c2d
Niels Nijens <nn@superbrave.nl>
2018-04-25 11:46:30 +0200

    Add transforming of scalar values based on a value map to AnnotationNormalizer

--- Tests/Serializer/Normalizer/AnnotationNormalizerTest.php
+++ Tests/Serializer/Normalizer/AnnotationNormalizerTest.php
@@ -158,7 +158,7 @@ class AnnotationNormalizerTest extends \PHPUnit_Framework_TestCase
                 'baz' => 1,
                 'qux' => array(),
                 'quuxs' => new ArrayCollection(),
-                'annotatedPropertyWithoutMethod' => true,
+                'annotatedPropertyWithoutMethod' => 'Yes',
             ),
             $normalizer->normalize($annotatedMock)
         );
